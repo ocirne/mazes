b1a19c3b91d364acd88eee287930bf485439e762
Enrico Pilz <kontakt@enricopilz.de>
2022-05-25 21:12:43 +0200

    Add cell class

---
+++ mazes-for-programmers/python/chapter_2/cell.py
@@ -0,0 +1,39 @@
+class Cell:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+        self.links = {}
+        self.north = None
+        self.south = None
+        self.east = None
+        self.west = None
+
+    def link(self, cell, bidi=True):
+        self.links[cell] = True
+        if bidi:
+            cell.link(self, False)
+        return self
+
+    def unlink(self, cell, bidi=True):
+        del self.links[cell]
+        if bidi:
+            cell.unlink(cell, False)
+        return self
+
+    def links(self):
+        return self.links.keys()
+
+    def is_linked(self, cell):
+        return cell in self.links
+
+    def neighbors(self):
+        list = []
+        if self.north is not None:
+            list.append(self.north)
+        if self.south is not None:
+            list.append(self.south)
+        if self.east is not None:
+            list.append(self.east)
+        if self.west is not None:
+            list.append(self.west)
+        return list
