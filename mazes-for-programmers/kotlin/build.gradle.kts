c3aecdc56111cda8e48e88ddc26a1dc5ea8c05e8
Enrico Pilz <kontakt@enricopilz.de>
2022-06-15 17:53:02 +0200

    Add Kotlin basics

---
+++ mazes-for-programmers/kotlin/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(platform("org.junit:junit-bom:5.7.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.kotest:kotest-runner-junit5:4.4.1")
+    testImplementation("io.kotest:kotest-assertions-core:4.4.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.4.31")
+    testImplementation("org.reflections:reflections:0.9.12")
+}
+
+configurations {
+    implementation {
+        resolutionStrategy.failOnVersionConflict()
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    test {
+        testLogging.showExceptions = true
+    }
+}
