d28f92fa671a9a6f37c769b4cf4d5f0f5af11f26
Enrico Pilz <kontakt@enricopilz.de>
2022-06-25 15:41:37 +0200

    wip

---
+++ mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/demos/RandomMaze.kt
@@ -0,0 +1,23 @@
+package io.github.ocirne.mazes.demos
+
+fun main() {
+    // Ausgabe Seed
+
+    // Auswahl des Grids
+
+    // Auswahl des Algorithmus
+    // Weave: optional
+
+    // DeadEndkiller
+    // Braid
+
+    // Colorisierung der Hintergründe
+    // Colorisierung der Wände
+
+    // Ausgabe:
+    // Insets
+
+    // Path
+
+    // Marker
+}
