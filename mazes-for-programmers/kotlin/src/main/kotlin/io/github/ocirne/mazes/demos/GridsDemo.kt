7a6285b81112dacd851487e378b1ce593f6cccb6
Enrico Pilz <kontakt@enricopilz.de>
2022-07-03 17:15:18 +0200

    Add grids to pages

--- mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/demos/GridsDemo.kt
+++ mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/demos/GridsDemo.kt
@@ -4,9 +4,9 @@ import io.github.ocirne.mazes.grids.*
 import io.github.ocirne.mazes.output.save
 
 fun main() {
-    CartesianGrid(11, 11).toImage().save("cartesian")
-    PolarGrid(11).toImage().save("polar")
-    TriangleGrid(11, 18).toImage().save("triangle")
-    HexGrid(10, 10).toImage().save("hex")
-    UpsilonGrid(11, 11).toImage().save("upsilon")
+    CartesianGrid(10, 10).toImage().save("cartesian")
+    PolarGrid(5).toImage().save("polar")
+    TriangleGrid(7, 10).toImage(cellSize=40).save("triangle")
+    HexGrid(6, 7).toImage().save("hex")
+    UpsilonGrid(7, 7).toImage(cellSize=15).save("upsilon")
 }
