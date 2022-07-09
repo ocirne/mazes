b940a8eb906d7aa9d2af91140f423ab883162289
Enrico Pilz <kontakt@enricopilz.de>
2022-07-09 13:56:49 +0200

    Fix binary tree for triangle grid

--- mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/algorithms/BinaryTree.kt
+++ mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/algorithms/BinaryTree.kt
@@ -29,7 +29,20 @@ class BinaryTree : PassageCarver {
 
     fun on(grid: TriangleGrid) {
         for (cell in grid.eachCell()) {
-            val neighbors = listOfNotNull(cell.north, cell.east)
+            val neighbors =
+                if (cell.isUpright()) {
+                    if (cell.east != null) {
+                        listOfNotNull(cell.east)
+                    } else {
+                        listOfNotNull(cell.west)
+                    }
+                } else {
+                    if (cell.east != null && cell.east!!.east == null) {
+                        listOfNotNull(cell.north)
+                    } else {
+                        listOfNotNull(cell.north, cell.east)
+                    }
+                }
             if (neighbors.isEmpty()) {
                 continue
             }
