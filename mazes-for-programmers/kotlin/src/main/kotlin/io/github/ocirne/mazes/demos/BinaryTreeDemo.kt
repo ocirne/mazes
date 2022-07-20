3c97169d4d81c21ba81e6953a22f6eb56afb711f
Enrico Pilz <kontakt@enricopilz.de>
2022-07-20 21:15:37 +0200

    Readd binary tree

---
+++ mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/demos/BinaryTreeDemo.kt
@@ -0,0 +1,16 @@
+package io.github.ocirne.mazes.demos
+
+import io.github.ocirne.mazes.algorithms.BinaryTree
+import io.github.ocirne.mazes.colorization.Colorization
+import io.github.ocirne.mazes.grids.CartesianGrid
+import io.github.ocirne.mazes.output.save
+
+
+fun main() {
+    val grid = CartesianGrid(21, 21)
+    BinaryTree().on(grid)
+    grid.toImage().save("cartesian_binarytree")
+
+    val colorization = Colorization(grid).dijkstra(grid[20, 10]!!)
+    grid.toImage(backgroundColors = colorization).save("cartesian_binarytree_colorized")
+}
