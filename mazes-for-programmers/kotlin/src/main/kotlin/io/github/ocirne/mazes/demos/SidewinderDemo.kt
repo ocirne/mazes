9f67bad218f6160fd0459ad258e9f7554c7f0527
Enrico Pilz <kontakt@enricopilz.de>
2022-09-27 22:52:11 +0200

    Sidewinder, kt

--- mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/demos/SidewinderDemo.kt
+++ mazes-for-programmers/kotlin/src/main/kotlin/io/github/ocirne/mazes/demos/SidewinderDemo.kt
@@ -3,16 +3,16 @@ package io.github.ocirne.mazes.demos
 import io.github.ocirne.mazes.algorithms.Sidewinder
 import io.github.ocirne.mazes.colorization.Colorization
 import io.github.ocirne.mazes.grids.cartesian.CartesianGrid
-import io.github.ocirne.mazes.output.saveAs
-
+import io.github.ocirne.mazes.grids.cartesian.createImage
+import io.github.ocirne.mazes.grids.cartesian.saveAs
 
 fun main() {
-    (1.. 3).forEach { i ->
-        val grid = CartesianGrid(21, 21)
-        val maze = Sidewinder().on(grid)
-        maze.toImage().saveAs("cartesian_sidewinder_$i")
+    val grid = CartesianGrid(11, 11)
+    val maze = Sidewinder().on(grid)
+    maze.createImage().saveAs("cartesian_sidewinder")
 
-        val colorization = Colorization(maze).dijkstra(maze[20, 10]!!)
-        maze.toImage(backgroundColors = colorization).saveAs("cartesian_sidewinder_${i}_colored")
-    }
+    val colorization = Colorization(maze).dijkstra(maze[5, 5]!!)
+    maze.createImage()
+        .withBackgroundColors(colorization)
+        .saveAs("cartesian_sidewinder_colored")
 }
